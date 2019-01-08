955e7385ed8da97eb0c097ad89e33a67ae89c370
Beautifulife <davidkim1018@gmail.com>
2019-01-08 19:27:57 +0900

    Make meetup-list

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ const $noticationButton = document.getElementsByClassName('header-noti')[0];
 const $searchBox = document.getElementById('search-box');
 const $listContentLayer = document.getElementsByClassName('list-content-layer')[0];
 const $searchToggle = document.getElementsByClassName('btn-search-toggle')[0];
+const $listContentWrapper = document.getElementsByClassName('list-content-wrapper')[0];
 const vanillaMeetup = {
   map: undefined,
   markerStorage: [],
@@ -47,6 +48,7 @@ function initMap() {
       vanillaMeetup.centerPosition = { lat: ev.latLng.lat(), lon: ev.latLng.lng() };
 
       searchAndCleansData();
+      $listContentLayer.classList.add('active');
     }
   });
 
@@ -61,12 +63,12 @@ $searchToggle.addEventListener('click', toggleSearchAndList);
 
 function toggleSearchAndList(ev) {
   if (ev.currentTarget.textContent === '모임 목록') {
-    $listContentLayer.classList.add('active');
     ev.currentTarget.textContent = '모임 검색';
   } else {
-    $listContentLayer.classList.remove('active');
     ev.currentTarget.textContent = '모임 목록';
   }
+
+  $listContentLayer.classList.toggle('active');
 }
 
 function searchAndCleansData(ev) {
@@ -106,29 +108,34 @@ function makeAjaxPromise(url) {
 function cleansData(meetupData) {
   console.log(meetupData);
   meetupData.forEach((data, index) => {
+    const position = {};
+    const hostName = [];
+    const hostPhoto = [];
+    const hostThumbnail = [];
+    const date = data.local_date + '  ' + data.local_time;
+
+    position.lat = data.venue ? data.venue.lat : data.group.lat;
+    position.lng = data.venue ? data.venue.lon : data.group.lon;
+    data.event_hosts.forEach(item => hostName.push(item.name));
+    data.event_hosts.forEach((item) => {
+      if (item.photo) {
+        hostPhoto.push(item.photo.photo_link);
+        hostThumbnail.push(item.photo.thumb_link);
+      } else {
+        hostPhoto.push('/assets/images/sub_img.png');
+        hostThumbnail.push('/assets/images/sub_img.png');
+      }
+    });
+
     vanillaMeetup.meetupData[index] = {
       title: data.name,
-      position: {
-        lat: data.venue ? data.venue.lat : data.group.lat,
-        lng: data.venue ? data.venue.lon : data.group.lon,
-      },
-      date: data.local_date,
-      time: data.local_time,
+      groupName: data.group.name,
+      position,
+      date,
       rvsp: { limit: data.rsvp_limit, yes: data.yes_rsvp_count },
-      hostName: data.event_hosts.reduce((acc, item) => {
-        acc.push(item.name);
-
-        return acc;
-      }, []),
-      hostPhoto: data.event_hosts.reduce((acc, item) => {
-        if (item.photo) {
-          acc.push(item.photo.photo_link);
-        } else {
-          acc.push('/assets/images/sub_img.png');
-        }
-
-        return acc;
-      }, []),
+      hostName,
+      hostPhoto,
+      hostThumbnail,
     };
   });
 }
@@ -139,6 +146,61 @@ function renderData(cleansedData) {
   }
 
   cleansedData.forEach(data => makeMarker(data));
+  cleansedData.forEach(data => makeListItem(data));
+}
+
+function makeListItem(data) {
+  const contentList = document.createElement('li');
+  const titleParagraph = document.createElement('p');
+  const groupParagraph = document.createElement('p');
+  const timeParagraph = document.createElement('p');
+  const photoWrapperDiv = document.createElement('div');
+  const rvspWrapperDiv = document.createElement('div');
+  const favoriteButtonSpan = document.createElement('span');
+  const rvspSpan = document.createElement('span');
+
+  contentList.classList.add('list-content');
+  titleParagraph.classList.add('list-item-title');
+  groupParagraph.classList.add('list-item-group');
+  timeParagraph.classList.add('list-item-time');
+  photoWrapperDiv.classList.add('list-item-photo-wrapper');
+  rvspWrapperDiv.classList.add('list-item-rvsp-wrapper');
+  favoriteButtonSpan.classList.add('btn-add-favorite');
+
+  titleParagraph.textContent = data.title;
+  groupParagraph.textContent = data.groupName;
+  timeParagraph.textContent = data.date;
+  rvspSpan.textContent = `${data.rvsp.yes}명이 참석 예정입니다.`;
+  favoriteButtonSpan.innerHTML = '<i class="fas fa-plus"></i>';
+
+  $listContentWrapper.appendChild(contentList);
+  contentList.appendChild(titleParagraph);
+  contentList.appendChild(groupParagraph);
+  contentList.appendChild(timeParagraph);
+  contentList.appendChild(photoWrapperDiv);
+  contentList.appendChild(rvspWrapperDiv);
+  contentList.appendChild(favoriteButtonSpan);
+  data.hostThumbnail.forEach((item) => {
+    const photoDiv = document.createElement('div');
+
+    photoDiv.classList.add('list-item-photo');
+    photoDiv.style.backgroundImage = `url('${item}')`;
+    photoWrapperDiv.appendChild(photoDiv);
+  });
+  rvspWrapperDiv.appendChild(rvspSpan);
+
+  if (data.rvsp.limit) {
+    const rvspSpan = document.createElement('span');
+    let limitMinusYes = data.rvsp.limit - data.rvsp.yes;
+
+    if (limitMinusYes < 0) {
+      limitMinusYes = 0;
+    }
+
+    rvspSpan.classList.add('list-item-rvsp');
+    rvspSpan.textContent = `자리가 ${limitMinusYes} 개 남았습니다.`;
+    rvspWrapperDiv.appendChild(rvspSpan);
+  }
 }
 
 function makeMarker(data) {
@@ -162,7 +224,7 @@ function makeMarker(data) {
   vanillaMeetup.markerStorage.push(marker);
 
   function makeContentForm(contents) {
-    return `<h1>${contents.title}</h1>`;
+    return `<h3>${contents.title}</h3>`;
   }
 }
 
